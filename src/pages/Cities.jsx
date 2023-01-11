04a451065b605a90703ec66b1143074ae05f25af
Tiago dos Santos Quadros <tiago@MacBook-Air-de-Tiago.local>
2023-01-11 14:54:08 -0300

    Mais comentários

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -15,6 +15,10 @@ class Cities extends Component {
       https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/095ebb0d-1932-4d37-933b-9e1d721646fb/section/1c36f886-88d1-424c-b7b4-d8350620a118/day/6bfa2e2e-4802-4b72-8aeb-b7b55ad63189/lesson/dd85a61c-c595-4a58-9601-5d6c38ad9f0b
     */
     const { uf } = this.props.match.params;
+    /*
+      Neste setState, nós possuímos uma função sendo passada como segundo parâmetro (uma callback).
+      A ideia é garantir que a função loadCities seja executada apenas após a conclusão do setState.
+    */
     this.setState({
       uf,
     }, this.loadCities);
