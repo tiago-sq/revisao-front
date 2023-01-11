7a4384c665e0a859db1394b16d38bc43a91a9ebe
Tiago dos Santos Quadros <tiago@MacBook-Air-de-Tiago.local>
2023-01-11 12:38:06 -0300

    Código Inicial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import DarkButton from './components/DarkButton';
 import Menu from './components/Menu';
 import Cities from './pages/Cities';
 import Home from './pages/Home';
@@ -8,24 +7,11 @@ import NotFound from './pages/NotFound';
 import Search from './pages/Search';
 import './index.css';
 class App extends Component {
-  state = { 
-    isDark: false,
-  } 
 
-  handleIsDark = () => {
-    this.setState(({ isDark }) => ({
-      isDark: !isDark,
-    }))
-  }
 
   render() { 
-    const { isDark } = this.state;
     return (
-      <div className={ isDark ? 'container dark' : 'container light' }>
-        <DarkButton
-          isDark={ isDark }
-          handleIsDark={ this.handleIsDark }
-        />
+      <div className="container">
         <Menu />
         <Switch>
           <Route exact path="/" component={ Home } />
